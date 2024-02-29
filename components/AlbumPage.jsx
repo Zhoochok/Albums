84f6edf55646ecd0251382c95d1cdab1af0ea03a
Vadim <bezvadimka@mail.ru>
2024-02-29 16:26:32 +0300

    norm

--- components/AlbumPage.jsx
+++ components/AlbumPage.jsx
@@ -1,12 +1,14 @@
 const React = require("react");
 const Layout = require("./Layout");
+const FormAddAlbum = require('./FormAddAlbum')
 
-module.exports = function Album({ title, album}) {
+module.exports = function Album({ title, album, user}) {
   return (
     <Layout title={title}>
+        <FormAddAlbum />
       <div className="albumPage">
         {album.map((el) => (
-          <a href={`/albums/${el.name}/photo`}>
+          <a href={`/albums/${el.id}/photo`}>
             <div className="albums">
               <p>
                 <img className="picture" src={el.img} />
