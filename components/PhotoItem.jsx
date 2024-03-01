a64b48a168e1b7ac03400a3d07538e6117801c09
Жуков Иван <152768804+Zhoochok@users.noreply.github.com>
2024-03-01 16:15:17 +0300

    vseDone

--- components/PhotoItem.jsx
+++ components/PhotoItem.jsx
@@ -1,13 +1,16 @@
 const React = require('react');
 
-function PhotoItem({ photo, album }) {
+function PhotoItem({ photo, album, user }) {
   return (
-
     <div className="card" data-id={photo.id}>
       <img src={photo.img} alt="photo" width={400} height={300} />
       <h3>{photo.description}</h3>
+      {user && photo.id === user.id && (
       <a href={`/photo/${photo.id}/update`}>Изменить</a>
-      <button className="btn-delete">Delete photo</button>
+      )}
+      {user && photo.id === user.id && (
+        <button className="btn-delete">Delete photo</button>
+      )}
     </div>
   );
 }
--- public/scripts/album.js
+++ public/scripts/album.js
@@ -1,8 +1,8 @@
-const formAddAlbum = document.querySelector(".album-form");
-const deleteAlbum = document.querySelector(".albumPage");
+const formAddAlbum = document.querySelector('.album-form');
+const deleteAlbum = document.querySelector('.albumPage');
 
 if (formAddAlbum) {
-  formAddAlbum.addEventListener("submit", async (event) => {
+  formAddAlbum.addEventListener('submit', async (event) => {
     event.preventDefault();
     const { name, img, privats } = event.target;
     const data = {
@@ -10,29 +10,29 @@ if (formAddAlbum) {
       img: img.value,
       privats: privats.value,
     };
-    const response = await fetch("/api/albums", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
+    const response = await fetch('/api/albums', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
       body: JSON.stringify(data),
     });
     const res = await response.json();
     if (res.message === 'success') {
       formAddAlbum.reset();
       document
-        .querySelector(".albumPage")
+        .querySelector('.albumPage')
         .insertAdjacentHTML('afterbegin', res.html);
     }
   });
 }
 
 if (deleteAlbum) {
-  deleteAlbum.addEventListener("click", async (event) => {
-    if (event.target.classList.contains("btn-deleteAlbum")) {
-      const card = event.target.closest(".albums");
+  deleteAlbum.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('btn-deleteAlbum')) {
+      const card = event.target.closest('.albumPage');
       const { albumid } = card.dataset;
-      const res = await fetch(`/api/albums/${albumid}`, { method: "DELETE" });
+      const res = await fetch(`/api/albums/${albumid}`, { method: 'DELETE' });
       const data = await res.json();
-      if (data.message === "success") {
+      if (data.message === 'success') {
         card.remove();
       }
     }
