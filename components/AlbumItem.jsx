b3210dfab638624daf0b9db6297b346743940e05
Жуков Иван <152768804+Zhoochok@users.noreply.github.com>
2024-03-01 16:39:09 +0300

    leGO

--- components/AlbumItem.jsx
+++ components/AlbumItem.jsx
@@ -4,10 +4,12 @@ module.exports = function AlbumItem({ title, album, user, url, el }) {
   return (
     <div className="albumPage" data-albumId={el.id}>
       <a href={`/albums/${el.id}/photo`}>
-        <p>
+        <p >
           <img className="picture" src={el.img} width={500} height={300} />
         </p>
+        <div className="textStr">
         {el.name}
+        </div>
       </a>
       {user && el.userId === user.id && (
         <button className="btn-deleteAlbum">Удалить</button>
