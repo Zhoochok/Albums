70f395420ae4bac1d130d289c42f1aaeb33930f6
Жуков Иван <152768804+Zhoochok@users.noreply.github.com>
2024-03-01 16:57:23 +0300

    goGO

--- components/PhotoItem.jsx
+++ components/PhotoItem.jsx
@@ -3,8 +3,8 @@ const React = require('react');
 function PhotoItem({ photo, album, user }) {
   return (
     <div className="card" data-id={photo.id}>
-      <img src={photo.img} alt="photo" width={400} height={300} />
-      <h3>{photo.description}</h3>
+      <img className='zdorovo'src={photo.img} alt="photo" width={400} height={300} />
+      <h3 className='textStr'>{photo.description}</h3>
       {user && photo.id === user.id && (
       <a href={`/photo/${photo.id}/update`}>Изменить</a>
       )}
